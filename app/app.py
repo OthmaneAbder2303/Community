4906e4230c578cd059b7b00e20a73899a298d75f
Othmane Abderrazik <othmane232004@gmail.com>
2025-01-26 17:46:00 +0100

    variables

--- app/app.py
+++ app/app.py
@@ -4,9 +4,19 @@ app = Flask(__name__)   # Flask constructor
   
 # A decorator used to tell the application 
 # which URL is associated function 
-@app.route('/')       
-def hello(): 
-    return 'OTHMANE'
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+#app.add_url_rule('/', 'hello', hello)
+
+@app.route('/hello/<name>') 
+def hello_name(name): 
+    return 'Hello %s!' % name 
 
 if __name__=='__main__': 
-   app.run() 
+   app.run(debug = True) 
