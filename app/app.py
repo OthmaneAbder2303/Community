65038cfd8c4eae24cc455258b7a687138db214b1
Othmane Abderrazik <othmane232004@gmail.com>
2025-01-26 17:03:13 +0100

    launching app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask     
+from flask import Flask      # type: ignore
+
 app = Flask(__name__)   # Flask constructor 
   
 # A decorator used to tell the application 
 # which URL is associated function 
 @app.route('/')       
 def hello(): 
-    return 'HELLO'
+    return 'OTHMANE'
 
 if __name__=='__main__': 
    app.run() 
