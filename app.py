4bebf4ab9bcd300cec025644ae88750e75e8b9f4
Othmane Abderrazik <othmane232004@gmail.com>
2025-02-12 14:50:40 +0100

    defining login in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, session
 from models import db,user
 
 app = Flask(__name__)   # Flask constructor 
@@ -14,9 +14,22 @@ def home():
         
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        log = request.form.get('login')
+        session['name'] = log
+        password = request.form.get('password')
+        usr = user.query.filter_by(login = log).first()
+        if usr and usr.password == password:
+            return redirect('chat')
+        else :
+            print('Not Authorized')
+            return redirect('login')
+    
     return render_template('login.html')
+
+
 
 if __name__=='__main__': 
    app.run(debug = True) 
